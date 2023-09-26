f583db5703ee1d298218900ef0bb078ee643929a
Tom Hall <thall1961@gmail.com>
2023-09-26 17:06:48 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Confetti from "react-confetti";
 import {useEffect, useRef, useState} from "react";
 import './App.css'
+import alarm from './alarm.mp3'
 
 
 const TOTAL_SECONDS = 5 * 60
@@ -10,6 +11,7 @@ function App() {
   const [secondsLeft, setSecondsLeft] = useState(TOTAL_SECONDS);
   const [going, setGoing] = useState(false);
   const windowSize = useRef([window.innerWidth, window.innerHeight]);
+  const audioRef = useRef(null);
 
   function showConfetti() {
     setDisplay('block')
@@ -21,22 +23,34 @@ function App() {
   function handleCheckboxClick(event) {
     setSecondsLeft(TOTAL_SECONDS)
     showConfetti();
+    stopTheMusic();
   }
 
   useEffect(() => {
     const interval = setInterval(() => {
       if (going) {
         setSecondsLeft(secondsLeft > 0 ? secondsLeft - 1 : 0)
+        if (secondsLeft <= 0) {
+          audioRef.current.play();
+        }
       }
     }, 1000);
 
     return () => clearInterval(interval);
   }, [going, secondsLeft]);
 
+  function stopTheMusic() {
+    audioRef.current.pause();
+    audioRef.current.currentTime = 0;
+  }
+
   return (
     <>
       <div style={{padding: '40px', fontFamily: 'arial, sans-serif', maxWidth: '800px', margin: '0 auto'}}>
-        <h1>TICKET TIMER</h1>
+        <div className="flex centered">
+          <h1>TICKET TIMER</h1>
+          <button className="btn secondary" onClick={() => setGoing(!going)}>Start/Stop &rarr;</button>
+        </div>
         <div className="flex apart">
           <div>
             <div style={{padding: 0, margin: 0}}>
@@ -52,8 +66,10 @@ function App() {
                 width={windowSize[0]}
                 height={windowSize[1]}
               />
-              <button className="btn primary" onClick={handleCheckboxClick}>Restart</button>
-              <button className="btn secondary" onClick={() => setGoing(!going)}>Start/Stop &rarr;</button>
+              <button className="btn btn-lg primary" onClick={handleCheckboxClick}>NEXT TICKET</button>
+              <audio style={{ marginTop: '200px' }} ref={audioRef} controls src={alarm}>
+              </audio>
+              <button className="btn primary" onClick={stopTheMusic}>STOP THE MUSIC</button>
             </div>
           </div>
         </div>
